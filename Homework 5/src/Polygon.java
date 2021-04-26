206a2062eccabe1e7b038bb44fa30039ac0d0d1c
Samsom <anenia.samson@gmail.com>
2021-04-25 21:00:54 -0500

    java

---
+++ Homework 5/src/Polygon.java
@@ -0,0 +1,15 @@
+public class Polygon {
+
+
+
+
+    public static int sd(int sides){
+        System.out.println("the side is: "+ sides);
+        return sides;
+    }
+    public static String co(String color){
+        System.out.println("The color is: " + color);
+        return color;
+    }
+
+}

b2470bf426079911aace26a00669ee7fcd4601a1
Samsom <anenia.samson@gmail.com>
2021-05-16 18:16:14 -0500

    java

--- Homework 7/src/Question2.java
+++ Homework 7/src/Question2.java
@@ -8,9 +8,8 @@ public class Question2 {
 
         //2.Given Two sets below two setsSet1: [3,4,5,7,8,9]Set2 [5,2,4,6,7,11]
         //a. Find the intersection in those sets
-        // b. Find the union in those sets
-        // c. Find the only unique elements in set2
 
+// The reason I copy the sets is because it override and it gives wrong value
 
         // Set 1
         Set<Integer> set1 = new HashSet<>();
@@ -30,12 +29,41 @@ public class Question2 {
         set2.add(7);
         set2.add(11);
 
-
+        // b. Find the union in those sets
         set2.retainAll(set1);
         System.out.println("Intersection " + set2);
-        set2.addAll(set1);
-        System.out.println("Union "+ set1);
+        // Set 1
+        Set<Integer> set3 = new HashSet<>();
+        set3.add(3);
+        set3.add(4);
+        set3.add(5);
+        set3.add(7);
+        set3.add(8);
+        set3.add(9);
 
+        // Set 2
+        Set<Integer> set4 = new HashSet<>();
+        set4.add(5);
+        set4.add(2);
+        set4.add(4);
+        set4.add(6);
+        set4.add(7);
+        set4.add(11);
+
+        set2.addAll(set1);
+        System.out.println("Union "+ set3);
+
+
+        // c. Find the only unique elements in set2
+        Set<Integer> set2_unique = new HashSet<>();
+        set2_unique.add(5);
+        set2_unique.add(2);
+        set2_unique.add(4);
+        set2_unique.add(6);
+        set2_unique.add(6);
+        set2_unique.add(7);
+        set2_unique.add(11);
+        System.out.println("The Unique elements in set2 is: " + set2_unique);
 
 
     }
